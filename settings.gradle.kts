f1461397ad2c0c41d75849d1938d7f83ea162830
CoderYellow <CoderYellow@hotmail.com>
2026-05-25 22:53:13 +0800

    chore: add conditional Maven repository configuration for non-CI environments

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,16 +2,36 @@ import java.io.File
 
 pluginManagement {
     repositories {
+        val isCi = System.getenv().containsKey("CI") ||
+                System.getenv().containsKey("GITHUB_ACTIONS") ||
+                System.getenv().containsKey("JENKINS_HOME")
+
+        if (!isCi) {
+            maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public/")
+            maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+        }
+
+        mavenCentral()
         gradlePluginPortal()
-        maven(url = "https://repo.spring.io/plugins-release")
     }
 }
 
 dependencyResolutionManagement {
     repositories {
+        val isCi = System.getenv().containsKey("CI") ||
+                System.getenv().containsKey("GITHUB_ACTIONS") ||
+                System.getenv().containsKey("JENKINS_HOME")
+
+        if (!isCi) {
+            maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public/")
+            maven(url = "https://maven.aliyun.com/repository/public/")
+        }
+
         mavenCentral()
+        gradlePluginPortal()
     }
 }
+
 
 rootProject.name = "karate-gradle"
 
